654854534b79cd175abaee97e09abfec2b64f3e1
banu <bowsiya22a@gmail.com>
2019-03-30 12:10:10 +0530

    created taskone

---
+++ src/org/cts/com/Company.java
@@ -0,0 +1,19 @@
+package org.cts.com;
+
+public class Company {
+public void comID(){
+	System.out.println("id is 23847");
+
+	
+}
+public void comName(){
+	System.out.println("comname is CTS");
+}
+public static void main(String[] args) {
+	Company ci = new Company();
+	ci.comID();
+	ci.comName();
+	
+	
+}
+}
